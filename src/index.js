a9518366058e30a0c817c2ca0b3bddcb7cf4a9c1
dfavo <favorskyd@gmail.com>
2020-03-15 21:14:22 -0700

    Add new folders assets and fonts and files to them

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import Post from "./Post";
+import json from "../assets/json.json";
+import WebpackLogo from "../assets/Webpack logo png.png";
 import "./styles/styles.css";
 
-const post = new Post("Webpack Post Title");
+const post = new Post("Webpack Post Title", WebpackLogo);
 
 console.log("Post to String: ", post.toString());
 
 // WebPack understand just javaScript, do not understand CSS etc
+console.log("JSON:", json);
